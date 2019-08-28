78ae0776b8efd3b0c3bbd6c8407a439fa7d2e833
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2019-08-28 17:05:59 +0800

    feat:1button 层级更改

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="segma-button">
-        <div class="b50-btn">
+    <div>
+
+        <div class="segma-button b50-btn">
             <el-button class="segma-xs primary">尺寸
             </el-button>
             <el-button class="segma-xs secondary">尺寸
@@ -8,7 +9,8 @@
             <el-button class="segma-xs subtle">尺寸
             </el-button>
         </div>
-        <div class="r50-btn">
+
+        <div class="segma-button r50-btn">
             <el-button class="segma-s primary">尺寸
             </el-button>
             <el-button class="segma-s secondary">尺寸
@@ -16,7 +18,7 @@
             <el-button class="segma-s subtle">尺寸
             </el-button>
         </div>
-        <div class="g50-btn">
+        <div class="segma-button g50-btn">
             <el-button class="segma-m primary">尺寸
             </el-button>
             <el-button class="segma-m secondary">尺寸
@@ -24,11 +26,12 @@
             <el-button class="segma-m subtle">尺寸
             </el-button>
         </div>
-        <div class="b50-btn">
+        <div class=" segma-button b50-btn">
             <el-button class="segma-s loading"><i class="el-icon-loading"></i></el-button>
             <el-button class="segma-s disabled">尺寸</el-button>
         </div>
     </div>
+
 </template>
 
 <script>
