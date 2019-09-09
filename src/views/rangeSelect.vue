0898d7ecb1c9161a79f7f9f10e001a976d9acf61
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2019-09-09 11:02:13 +0800

    feat:rangeSelect 组件路径文件改变

--- src/views/rangeSelect.vue
+++ src/views/rangeSelect.vue
@@ -1,11 +1,9 @@
 <template>
-    <div>
-        <range-select-bar :userCutList="importanceData.cutList"
-                          :min="importanceData.min"
-                          :max="importanceData.max"
-                          @changeData="getCutData">
-        </range-select-bar>
-    </div>
+    <range-select-bar :userCutList="cutList"
+                      :min="min"
+                      :max="max"
+                      @changeData="getCutData">
+    </range-select-bar>
 </template>
 <script>
 import rangeSelectBar from '../components/rangeSelectBar'
@@ -14,17 +12,18 @@ export default {
     components: { rangeSelectBar },
     data() {
         return {
-            importanceData: {
-                cutList: [
-                    0.5,
-                    0.9
-                ], min: 0, max: 1
-            }
+            cutList: [
+                0.5,
+                0.9
+            ],
+            min: 0,
+            max: 1
+
         }
     },
     methods: {
         getCutData(val) {
-            this.importanceData.cutList = val
+            this.cutList = val
         },
     },
 }
