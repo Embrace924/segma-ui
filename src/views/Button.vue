85d62c05ebde280cb46e44d5e7bc9002882a41ac
Embrace123 <631518164@qq.com>
2019-12-09 14:55:26 +0800

    feat:按钮各个样式

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,41 +1,83 @@
 <template>
     <div>
         <el-button type="primary"
+                   size="mini">尺寸
+        </el-button>
+        <el-button type="primary"
                    size="small">尺寸
+        </el-button>
+        <el-button type="primary"
+                   size="middle">尺寸
         </el-button>
         <el-button type="primary"
                    size="middle"
                    plain>尺寸
         </el-button>
-        <el-button disabled
+        <el-button type="primary"
+                   disabled>尺寸
+        </el-button>
+        <el-button disabled>尺寸</el-button>
+        <br>
+        <el-button type="danger"
                    size="mini">尺寸
         </el-button>
         <el-button type="danger"
+                   size="small">尺寸
+        </el-button>
+        <el-button type="danger"
+                   size="middle">尺寸
+        </el-button>
+        <el-button type="danger"
+                   size="middle"
                    plain>尺寸
         </el-button>
         <el-button type="danger"
-                   size="small">尺寸
+                   disabled>尺寸
+        </el-button>
+        <el-button disabled>尺寸</el-button>
+        <br>
+        <el-button type="success"
+                   size="mini">尺寸
         </el-button>
         <el-button type="success"
                    size="small">尺寸
         </el-button>
         <el-button type="success"
-                   plain
-                   size="mini">尺寸
+                   size="middle">尺寸
         </el-button>
+        <el-button type="success"
+                   size="middle"
+                   plain>尺寸
+        </el-button>
+        <el-button type="danger"
+                   disabled>尺寸
+        </el-button>
+        <el-button disabled>尺寸</el-button>
+        <br>
         <el-button type="warning"
                    size="mini">尺寸
         </el-button>
         <el-button type="warning"
-                   plain
-                   size="mini">尺寸
+                   size="small">尺寸
         </el-button>
+        <el-button type="warning"
+                   size="middle">尺寸
+        </el-button>
+        <el-button type="warning"
+                   size="middle"
+                   plain>尺寸
+        </el-button>
+        <el-button type="warning"
+                   disabled>尺寸
+        </el-button>
+        <el-button disabled>尺寸</el-button>
+
     </div>
 </template>
 
 <script>
 export default {
-	name: "Button"
+    name: "Button"
 }
 </script>
 
