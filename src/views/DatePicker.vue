29b64af9d5a7caebcfbdcdea0760888f3013c028
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-04-29 15:19:35 +0800

    feat(form系列):时间选择器样式

---
+++ src/views/DatePicker.vue
@@ -0,0 +1,34 @@
+<!--DatePicker-->
+<template>
+    <div class="DatePicker">
+        <el-time-picker v-model="value1"
+                        size="small"
+                        placeholder="任意时间点">
+        </el-time-picker>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DatePicker',
+    // components: {},
+    // directives: {},
+    // filters: {},
+    // model: {},
+    // props: [],
+    data() {
+        return {
+            value1: ''
+        }
+    },
+    // computed: {},
+    // watch: {},
+    // mounted() {},
+    // created() {},
+    // activated() {},
+    // beforeDestroy() {},
+    // destroyed() {},
+    // methods: {},
+};
+</script>
+
