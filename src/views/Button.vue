85bb03c70632ba2e5a99d6336418f4a3a887a102
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-05-06 14:18:22 +0800

    feat(button):按钮样式

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,33 +1,56 @@
 <template>
     <div>
-        <el-button plain
-                   size="small">default
+        <el-button type="primary"
+                   size="small">主按钮normal
         </el-button>
-        <el-button :loading="true"
-                   plain
-                   size="small">loading
+        <el-button type="primary"
+                   size="small"
+                   icon="el-icon-search">主按钮带icon
         </el-button>
         <el-button disabled
-                   size="small">disabled
-        </el-button>
-
-        <br>
-        <el-button type="primary"
-                   size="small">按钮
+                   type="primary"
+                   size="small">主按钮disabled
         </el-button>
         <el-button type="primary"
-                   size="small"
-                   plain>次要
-        </el-button>
-        <el-button type="primary"
-                   size="small"
-                   :loading="true">
-        </el-button>
-        <el-button type="primary"
-                   size="small"
                    :loading="true"
-                   plain>
+                   size="small">主按钮loading
         </el-button>
+        <br>
+        <el-button type="info"
+                   size="small">次按钮normal
+        </el-button>
+        <el-button type="info"
+                   size="small"
+                   icon="el-icon-search">次按钮带icon
+        </el-button>
+        <el-button disabled
+                   type="info"
+                   size="small">次按钮disabled
+        </el-button>
+        <el-button type="info"
+                   :loading="true"
+                   size="small">次按钮loading
+        </el-button>
+        <br>
+        <el-button size="small">普通按钮normal</el-button>
+
+        <el-button size="small"
+                   icon="el-icon-search">普通按钮带icon
+        </el-button>
+        <el-button disabled
+                   size="small">普通按钮disable
+        </el-button>
+        <el-button :loading="true"
+                   size="small">普通按钮loading
+        </el-button>
+        <br>
+
+
+        <!--<el-button size="small"-->
+        <!--icon="el-icon-search"-->
+        <!--disabled>带icon按钮-->
+        <!--</el-button>-->
+
 
     </div>
 </template>
