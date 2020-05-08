f43f65d23aaae63fa9ac82d3434389f572e2e878
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-05-08 17:03:22 +0800

    feat(Picker):时间选择样式更改

--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -5,6 +5,41 @@
                         size="small"
                         placeholder="任意时间点">
         </el-time-picker>
+        <br>
+        <el-date-picker v-model="value2"
+                        type="date"
+                        size="small"
+                        placeholder="选择日期"
+                        :picker-options="pickerOptionsData">
+        </el-date-picker>
+        <br>
+        <el-date-picker v-model="value3"
+                        type="year"
+                        size="small"
+                        placeholder="选择年">
+        </el-date-picker>
+        <br>
+        <el-date-picker v-model="value4"
+                        type="month"
+                        size="small"
+                        placeholder="选择月">
+        </el-date-picker>
+        <br>
+        <el-date-picker v-model="value5"
+                        type="week"
+                        size="small"
+                        format="yyyy 第 WW 周"
+                        placeholder="选择周">
+        </el-date-picker>
+        <br>
+        <el-date-picker v-model="value6"
+                        type="daterange"
+                        size="small"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        :picker-options="pickerOptionsRange">
+        </el-date-picker>
     </div>
 </template>
 
@@ -18,7 +53,82 @@ export default {
     // props: [],
     data() {
         return {
-            value1: ''
+            value1: '',
+            value2: '',
+            value3: '',
+            value4: '',
+            value5: '',
+            value6: '',
+
+            pickerOptionsData: {
+                shortcuts: [
+                    {
+                        text: '今天',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    },
+                    {
+                        text: '昨天',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit('pick', date);
+                        }
+                    },
+                    {
+                        text: '一周前',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', date);
+                        }
+                    }
+                ]
+            },
+            pickerOptionsRange: {
+                disabledDate(time) {
+                    return time.getTime() < Date.now();
+                },
+                shortcuts: [
+                    {
+                        text: '最近一周',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [
+                                start,
+                                end
+                            ]);
+                        }
+                    },
+                    {
+                        text: '最近一月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [
+                                start,
+                                end
+                            ]);
+                        }
+                    },
+                    {
+                        text: '最近三月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [
+                                start,
+                                end
+                            ]);
+                        }
+                    }
+                ]
+            },
         }
     },
     // computed: {},
@@ -31,4 +141,10 @@ export default {
     // methods: {},
 };
 </script>
+<style scoped
+       lang="less">
+/deep/ .el-date-editor {
+    margin-bottom: 20px;
+}
+</style>
 
