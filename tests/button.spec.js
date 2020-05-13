3b9303160f2ae5ddd082733087227057da713745
liuliu <815835778@qq.com>
2020-05-13 17:06:14 +0800

    feat(測試):部分測試用例

--- tests/button.spec.js
+++ tests/button.spec.js
@@ -1,177 +1,136 @@
+import { createTest, createVue, destroyVM } from './util';
+import Element from 'element-ui';
 import Vue from 'vue'
-import { expect } from 'chai'
-import Element from 'element-ui'
-import { mount } from '@vue/test-utils'
-import {
-    createVue,
-    destroyVM,
-    triggerEvent,
-    waitImmediate
-} from './util';
-import test from './test.vue'
-import '../src/assets/css/index.less'
-import './var.less'
-Vue.use(Element)
-describe('Input', () => {
-    let vm;
-    let primaryColor = 'rgb(83, 116, 192)';
-    let primaryFontColor = 'rgb(255, 255, 255)'
-    let primaryHoverColor = 'rgb(54, 75, 124)'
+Vue.use(Element);
+let Button  = Element.Button;
 
-    let primaryDisabledColor = 'rgb(233, 235, 240)'
-    let primaryDisabledFontColor = 'rgb(170, 175, 181)'
+describe('Button', () => {
+  let vm;
+  afterEach(() => {
+    destroyVM(vm);
+  });
 
-    it('主按鈕normal', async () => {
-        vm = createVue({
-            template: `
-            <el-button id="create" 
-                       type="primary" 
-                       size="small" 
-                       :class="{'hover' : isEnter}"
-                       @mouseenter="mouseenter"  
-                       @mouseleave="mouseleave">主按钮normal
-            </el-button>
-            `,
-            data () {
-                return {
-                    isEnter: false
-                }
-            },
-            methods: {
-                mouseenter () {
-                    this.isEnter = true;
-                },
-                mouseleave () {
-                    this.isEnter = false;
-                }
-            }
-        }, true);
-        const button = document.getElementById('create');
-        expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryColor);
-        expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryColor);
-        expect(window.getComputedStyle(button, null).fontSize).to.equal('14px');
-        expect(window.getComputedStyle(button, null).padding).to.equal('7px 16px');
-        expect(window.getComputedStyle(button, null).borderRadius).to.equal('4px');
-        expect(window.getComputedStyle(button, null).height).to.equal('30px');
-        expect(window.getComputedStyle(button, null).color).to.equal(primaryFontColor);
-        expect(vm.isEnter).to.be.false;
-        triggerEvent(button, 'mouseenter');
-        setTimeout(_ => {
-            expect(vm.isEnter).to.be.true;
-            expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryHoverColor);
-            expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryHoverColor);
-            done();
-        }, 300);
+  it('create', () => {
+    vm = createTest(Button, {
+      type: 'primary'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('el-button--primary')).to.be.true;
+  });
+  it('icon', () => {
+    vm = createTest(Button, {
+      icon: 'el-icon-search'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.querySelector('.el-icon-search')).to.be.ok;
+  });
+  it('nativeType', () => {
+    vm = createTest(Button, {
+      nativeType: 'submit'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.getAttribute('type')).to.be.equal('submit');
+  });
+  it('loading', () => {
+    vm = createTest(Button, {
+      loading: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-loading')).to.be.true;
+    expect(buttonElm.querySelector('.el-icon-loading')).to.be.ok;
+  });
+  it('disabled', () => {
+    vm = createTest(Button, {
+      disabled: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-disabled')).to.be.true;
+  });
+  it('size', () => {
+    vm = createTest(Button, {
+      size: 'medium'
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('el-button--medium')).to.be.true;
+  });
+  it('plain', () => {
+    vm = createTest(Button, {
+      plain: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-plain')).to.be.true;
+  });
+  it('round', () => {
+    vm = createTest(Button, {
+      round: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-round')).to.be.true;
+  });
+  it('circle', () => {
+    vm = createTest(Button, {
+      circle: true
+    }, true);
+    let buttonElm = vm.$el;
+    expect(buttonElm.classList.contains('is-circle')).to.be.true;
+  });
+  it('click', done => {
+    let result;
+    vm = createVue({
+      template: `
+        <el-button @click="handleClick"></el-button>
+      `,
+      methods: {
+        handleClick(evt) {
+          result = evt;
+        }
+      }
+    }, true);
+    vm.$el.click();
 
-        triggerEvent(button, 'mouseleave');
-        setTimeout(_ => {
-            expect(vm.isEnter).to.be.false;
-            expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryColor);
-            expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryColor);
-            done();
-        }, 300);
-    });
-    it('主按鈕帶上icon', () => {
-        vm = createVue({
-            template: `
-            <el-button id="create" 
-                       type="primary" 
-                       icon="el-icon-search" 
-                       size="small" 
-                       :class="{'hover' : isEnter}"
-                       @mouseenter="mouseenter"  
-                       @mouseleave="mouseleave">
-                       主按钮normal
-            </el-button>
-            `,
-            data () {
-                return {
-                    isEnter: false
-                }
-            },
-            methods: {
-                mouseenter () {
-                    this.isEnter = true;
-                },
-                mouseleave () {
-                    this.isEnter = false;
-                }
-            }
-        }, true);
-        const button = document.getElementById('create');
-        const icon = vm.$el.querySelector('.el-icon-search');
-        expect(icon).to.exist;
-        expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryColor);
-        expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryColor);
-        expect(window.getComputedStyle(button, null).fontSize).to.equal('14px');
-        expect(window.getComputedStyle(button, null).padding).to.equal('7px 16px');
-        expect(window.getComputedStyle(button, null).borderRadius).to.equal('4px');
-        expect(window.getComputedStyle(button, null).height).to.equal('30px');
-        expect(window.getComputedStyle(button, null).color).to.equal(primaryFontColor);
+    setTimeout(_ => {
+      expect(result).to.exist;
+      done();
+    }, 20);
+  });
 
-        expect(vm.isEnter).to.be.false;
-        triggerEvent(button, 'mouseenter');
-        setTimeout(_ => {
-            expect(vm.isEnter).to.be.true;
-            expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryHoverColor);
-            expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryHoverColor);
-            done();
-        }, 300);
+  it('click inside', done => {
+    let result;
+    vm = createVue({
+      template: `
+        <el-button @click="handleClick"><span class="inner-slot"></span></el-button>
+      `,
+      methods: {
+        handleClick(evt) {
+          result = evt;
+        }
+      }
+    }, true);
+    vm.$el.querySelector('.inner-slot').click();
 
-        triggerEvent(button, 'mouseleave');
-        setTimeout(_ => {
-            expect(vm.isEnter).to.be.false;
-            expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryColor);
-            expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryColor);
-            done();
-        }, 300);
-    });
+    setTimeout(_ => {
+      expect(result).to.exist;
+      done();
+    }, 20);
+  });
 
-    it.only('主按鈕disabled', () => {
-        vm = createVue({
-            template: `
-            <el-button id="create"
-                       type="primary"
-                       disabled>
-                       主按钮normal
-            </el-button>
-            `,
-        }, true);
-        const button = document.getElementById('create');
-        const icon = vm.$el.querySelector('.el-icon-search');
-        expect(icon).to.not.exist;
-        expect(window.getComputedStyle(button, null).backgroundColor).to.equal(primaryDisabledColor);
-        expect(window.getComputedStyle(button, null).borderColor).to.equal(primaryDisabledColor);
-        expect(window.getComputedStyle(button, null).fontSize).to.equal('14px');
-        expect(window.getComputedStyle(button, null).padding).to.equal('7px 16px');
-        expect(window.getComputedStyle(button, null).borderRadius).to.equal('4px');
-        expect(window.getComputedStyle(button, null).height).to.equal('30px');
-        expect(window.getComputedStyle(button, null).color).to.equal(primaryDisabledFontColor);
-    });
+  it('loading implies disabled', done => {
+    let result;
+    vm = createVue({
+      template: `
+        <el-button loading @click="handleClick"><span class="inner-slot"></span></el-button>
+      `,
+      methods: {
+        handleClick(evt) {
+          result = evt;
+        }
+      }
+    }, true);
+    vm.$el.querySelector('.inner-slot').click();
 
-    it('主按鈕loading', async () => {
-        vm = createVue({
-            template: `
-            <el-button id="create"
-                       :loading="loading"
-                       type="primary">
-                       主按钮normal
-            </el-button>
-            `,
-            data () {
-                return {
-                    loading: false
-                }
-            }
-        }, true);
-        const button = document.getElementById('create');
-        const icon = vm.$el.querySelector('.el-icon-loading');
-        expect(icon).to.exist;
-        expect(vm.loading).to.be.true;
-        vm.loading = false;
-        setTimeout(() => {
-            expect(icon).to.not.exist;
-            expect(vm.loading).to.be.false;
-        }, 300);
-    });
-})
+    setTimeout(_ => {
+      expect(result).to.not.exist;
+      done();
+    }, 20);
+  });
+});
