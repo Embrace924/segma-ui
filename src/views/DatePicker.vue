616be474d18aba6a0fe2a07bc60d41b2baec2e29
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-05-15 16:38:19 +0800

    feat(校验):v2.2固定时间下拉选项  效果

--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -50,6 +50,26 @@
                         end-placeholder="结束日期"
                         :picker-options="pickerOptionsRange">
         </el-date-picker>
+
+        <el-time-select size="small"
+                        placeholder="起始时间"
+                        v-model="startTime"
+                        :picker-options="{
+      start: '08:30',
+      step: '00:15',
+      end: '18:30'
+    }">
+        </el-time-select>
+        <el-time-select size="small"
+                        placeholder="结束时间"
+                        v-model="endTime"
+                        :picker-options="{
+      start: '08:30',
+      step: '00:15',
+      end: '18:30',
+      minTime: startTime
+    }">
+        </el-time-select>
     </div>
 </template>
 
@@ -69,7 +89,8 @@ export default {
             value4: '',
             value5: '',
             value6: '',
-
+            startTime: '',
+            endTime: '',
             pickerOptionsData: {
                 shortcuts: [
                     {
