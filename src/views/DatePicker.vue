1ac403b6817e0db0647dca3e67c982307d9ceefe
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-05-15 16:27:25 +0800

    feat(校验):v2.2固定时间下拉选项  效果

--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -1,6 +1,16 @@
 <!--DatePicker-->
 <template>
     <div class="DatePicker">
+        <el-time-select size="small"
+                        v-model="value1"
+                        :picker-options="{
+                            start: '08:30',
+                            step: '00:15',
+                            end: '18:30'
+                          }"
+                        placeholder="选择时间">
+        </el-time-select>
+
         <el-time-picker v-model="value1"
                         size="small"
                         placeholder="任意时间点">
