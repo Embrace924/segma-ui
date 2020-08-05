767ac52ab4b6c555c55c410d677be5a7b5c0a412
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-08-05 15:56:21 +0800

    feat(drawer):待更新样式

---
+++ src/views/Drawer.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <el-radio-group v-model="direction">
+            <el-radio label="ltr">从左往右开</el-radio>
+            <el-radio label="rtl">从右往左开</el-radio>
+            <el-radio label="ttb">从上往下开</el-radio>
+            <el-radio label="btt">从下往上开</el-radio>
+        </el-radio-group>
+
+        <el-button @click="drawer = true"
+                   type="primary"
+                   size="small"
+                   style="margin-left: 16px;">
+            点我打开
+        </el-button>
+
+        <el-drawer title="我是标题"
+                   :visible.sync="drawer"
+                   :direction="direction"
+                   :before-close="handleClose">
+            <span>我来啦!</span>
+        </el-drawer>
+        <br>
+        <el-button @click="drawer2 = true"
+                   type="primary"
+                   style="margin-left: 16px;">
+            点我打开
+        </el-button>
+        <el-drawer title="我是标题"
+                   :visible.sync="drawer2"
+                   :with-header="false">
+            <span>我来啦!</span>
+        </el-drawer>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            drawer2: false,
+            direction: 'rtl',
+        };
+    },
+    methods: {
+        handleClose(done) {
+            this.$confirm('确认关闭？')
+                .then(_ => {
+                    done();
+                })
+                .catch(_ => {
+                });
+        }
+    }
+};
+</script>
+<style lang="less"
+       scoped>
+
+</style>
